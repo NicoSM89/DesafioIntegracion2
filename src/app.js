db708ddcda4051600a1f6e6fd8d720acdab765f8
NicoSM89 <nsalamanca.murgas@gmail.com>
2024-02-13 11:29:30 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import session from 'express-session';
+import { secret } from './configurations/consts.js';
+import MongoStore from 'connect-mongo';
+import mongoose from 'mongoose';
+import viewsRoutes from './routes/views.routes.js';
+import sessionRoutes from './routes/session.routes.js';
+import initialePassport from './configurations/passport.config.js';
+import passport from 'passport';
+
+const PORT = 8080;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+const hbs = handlebars.create({
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true
+    }
+});
+
+mongoose.connect('mongodb+srv://nsalamanca:sammy123@cluster0.rwbs3lx.mongodb.net/coder');
+
+app.engine('handlebars', hbs.engine);
+app.set('views', 'src/views');
+app.set('view engine', 'handlebars');
+
+app.use(session({
+    secret: secret,
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://nsalamanca:sammy123@cluster0.rwbs3lx.mongodb.net/coder'
+    }),
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use('/', viewsRoutes);
+app.use('/api/session', sessionRoutes);
+
+initialePassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
